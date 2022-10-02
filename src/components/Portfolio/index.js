8032b5e768602ee8fe1f3bacc6ba2e8046b365d8
NickSchenck <yoyo4games@yahoo.com>
2022-10-02 18:59:54 -0500

    added project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PG1 from '../../assets/images/Boozy-Books.png'
 import PG2 from '../../assets/images/fictional-guacamole.png'
-import cat from '../../assets/images/Cute.jpg'
+import PG3 from '../../assets/images/whats-soap.PNG'
 
 function Portfolio() {
   
@@ -10,7 +10,7 @@ function Portfolio() {
     <div className="flex-row">
     <img alt='A book and drink pairing app' src={PG1} className="img-thumbnail mx-1" style={{ width: "50%" }} onClick={event =>  window.location.href='https://github.com/NickSchenck/Book-Nook'}></img>
     <img alt='A pet boarding app' src={PG2} className="img-thumbnail mx-1" style={{ width: "50%" }} onClick={event =>  window.location.href='https://github.com/NickSchenck/fictional-guacamole'}></img>
-    <img alt='A cute cat' src={cat} className="img-thumbnail mx-1" style={{ width: "30%" }} onClick={event =>  window.location.href='https://github.com/NickSchenck/Project1placeholder'}></img>
+    <img alt='A soap making app' src={PG3} className="img-thumbnail mx-1" style={{ width: "50%" }} onClick={event =>  window.location.href='https://github.com/AlexandertheGreat491/whats_soap'}></img>
     </div>
   );
 }
